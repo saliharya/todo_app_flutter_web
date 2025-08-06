ef7807f69e30b5b4c598bd6322187e535b28655e
saliharya <saliharyazinc@gmail.com>
2025-08-06 14:20:21 +0700

    Feat: Implement search functionality
    This commit introduces a search bar to the Todo list, allowing users to filter todos by title.
    
    Key changes:
    - **`SearchBarWidget`:** Added a new `SearchBarWidget` to `lib/presentation/widgets/search_bar_widget.dart` which is a `TextField` with search icon and customizable hint text.
    - **`TodoCubit`:**
        - Added `_allTodos` list to store the original list of todos.
        - Implemented `searchTodos(String query)` method to filter `_allTodos` based on the query and emit a new `TodoLoaded` state with the filtered list and the current query.
    - **`TodoState`:**
        - Added `query` property to `TodoLoaded` state to store the current search query.
    - **`TodoListPage`:**
        - Integrated the `SearchBarWidget`. The `onChanged` callback now calls `context.read<TodoCubit>().searchTodos`.
        - Wrapped the `ListView.builder` in a `Column` to accommodate the search bar.
        - Updated the condition for displaying "No todos found" to consider if the list is empty and the search query is also empty.
    - **Removed `search_bar.dart`:** The unused `lib/presentation/widgets/search_bar.dart` file was deleted.

--- lib/presentation/pages/todo_list_page.dart
+++ lib/presentation/pages/todo_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:todo_app_web/presentation/blocs/todo_cubit.dart';
 import 'package:todo_app_web/presentation/blocs/todo_state.dart';
 import 'package:todo_app_web/presentation/widgets/add_todo_modal.dart';
@@ -28,49 +29,69 @@ class TodoListPage extends StatelessWidget {
             return const Center(child: CircularProgressIndicator());
           } else if (state is TodoLoaded) {
             if (state.todos.isEmpty && state.query.isEmpty) {
-              return const Center(child: Text('No todos found.'));
+              return Column(
+                children: [
+                  SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: SvgPicture.asset(
+                      "assets/images/Detective-check-footprint 1.svg",
+                      errorBuilder: (context, error, stackTrace) {
+                        return const Icon(Icons.broken_image);
+                      },
+                    ),
+                  ),
+                  const Center(child: Text('Empty...')),
+                ],
+              );
             }
-            return Column(
-              children: [
-                const SizedBox(height: 16),
-                SearchBarWidget(
-                  onChanged: context.read<TodoCubit>().searchTodos,
-                ),
-                const SizedBox(height: 16),
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: state.todos.length,
-                  itemBuilder: (context, index) {
-                    final todo = state.todos[index];
-                    return TodoItem(
-                      todo: todo,
-                      onEdit: () {
-                        showEditTodoModal(context, todo.title, (updatedTitle) {
-                          final updatedTodo = Todo(
-                            id: todo.id,
-                            title: updatedTitle,
-                            completed: todo.completed,
-                          );
-                          context.read<TodoCubit>().updateTodo(updatedTodo);
-                        });
-                      },
-                      onDelete: () {
-                        context.read<TodoCubit>().deleteTodo(
-                          todo.id.toString(),
-                        );
-                      },
-                      onCompleteChange: (newValue) {
-                        final updatedTodo = Todo(
-                          id: todo.id,
-                          title: todo.title,
-                          completed: newValue,
-                        );
-                        context.read<TodoCubit>().updateTodo(updatedTodo);
-                      },
-                    );
-                  },
-                ),
-              ],
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 400),
+              child: Column(
+                children: [
+                  const SizedBox(height: 16),
+                  SearchBarWidget(
+                    onChanged: context.read<TodoCubit>().searchTodos,
+                  ),
+                  const SizedBox(height: 16),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: state.todos.length,
+                    itemBuilder: (context, index) {
+                      final todo = state.todos[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                        child: TodoItem(
+                          todo: todo,
+                          onEdit: () {
+                            showEditTodoModal(context, todo.title, (updatedTitle) {
+                              final updatedTodo = Todo(
+                                id: todo.id,
+                                title: updatedTitle,
+                                completed: todo.completed,
+                              );
+                              context.read<TodoCubit>().updateTodo(updatedTodo);
+                            });
+                          },
+                          onDelete: () {
+                            context.read<TodoCubit>().deleteTodo(
+                              todo.id.toString(),
+                            );
+                          },
+                          onCompleteChange: (newValue) {
+                            final updatedTodo = Todo(
+                              id: todo.id,
+                              title: todo.title,
+                              completed: newValue,
+                            );
+                            context.read<TodoCubit>().updateTodo(updatedTodo);
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             );
           } else if (state is TodoError) {
             return Center(child: Text('Error: ${state.message}'));
@@ -79,13 +100,16 @@ class TodoListPage extends StatelessWidget {
           }
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showAddTodoModal(context, (title) {
-            context.read<TodoCubit>().addTodo(title);
-          });
-        },
-        child: const Icon(Icons.add),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 350.0, vertical: 64),
+        child: FloatingActionButton(
+          onPressed: () {
+            showAddTodoModal(context, (title) {
+              context.read<TodoCubit>().addTodo(title);
+            });
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
